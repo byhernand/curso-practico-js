0b6eea58a7b37d53626413ab4594dab8d79e8313
Eduardo Hernand <byhernand@hotmail.com>
2022-02-19 20:34:38 -0600

    Reto 2 completado

--- descuentos.js
+++ descuentos.js
@@ -1,18 +1,35 @@
-function calcularPrecioConDescuento(precio, descuento) {
-  const porcentajePrecioConDescuento = 100 - descuento;
-  const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+const coupons = [
+  { promoCode: "MAGIKARP-COD3", discountValue: 10 },
+  { promoCode: "PSYDUCK-COD3", discountValue: 25 },
+  { promoCode: "HITMONLEE-COD3", discountValue: 40 },
+  { promoCode: "SNORLAX-COD3", discountValue: 60 },
+  { promoCode: "VENUSAUR-COD3", discountValue: 90 }
+]
 
-  return precioConDescuento;
+function calculatePriceWithDiscount(price, discount) {
+  const percentagePriceDiscount = 100 - discount;
+  const priceWithDiscount = (price * percentagePriceDiscount) / 100;
+
+  return priceWithDiscount;
 }
 
-function priceDiscount(){
+function calculateFinalPrice() {
   const inputPrice = document.getElementById("inputPrice");
-  const priceValue = inputPrice.value;
-  const inputDiscount = document.getElementById("inputDiscount");
-  const discountValue = inputDiscount.value;
-
-  const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
-
+  const inputCoupon = document.getElementById("inputCoupon");
+  const couponCode = inputCoupon.value;
   const resultP = document.getElementById("resultPrice");
-  resultP.innerText = "El precio con descuento son : $" + precioConDescuento;
+
+  const validCoupon = coupons.find( (coupon) => {
+    return couponCode === coupon.promoCode;
+  })
+  
+  if(validCoupon) {
+    const price = inputPrice.value;
+    const discount = validCoupon.discountValue;
+    const finalPrice = calculatePriceWithDiscount(price, discount);
+    resultP.innerHTML = `Tienes un descuento del ${discount}%, su precio a final es ${finalPrice}`;
+  }
+  else {
+    resultP.innerHTML = "El cupon no es valido";
+  }
 }
